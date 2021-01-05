9b951bd5acef0306f5e648b6c2375391e58ae5ca
heeyunkwon <76425337+heeyunkwon@users.noreply.github.com>
2021-01-05 21:12:24 +0900

    Update Labeling.py
    다음 이미지로 이동

--- Labeling.py
+++ Labeling.py
@@ -8,18 +8,23 @@ class Canvas(QLabel):
         super().__init__()
 
         self.initUI()
+        self.cnt = 0
+        self.fPixmap = QPixmap()
 
+        # 이미지 레이블
         self.ImgLabel = QLabel(self)
         self.ImgLabel.move(30, 30)
 
+        # 디렉터리 경로 표시 레이블
         self.dirlabel = QLabel(self)
         self.dirlabel.resize(500, 50)
         self.dirlabel.move(150, 650)
         self.dirlabel.setStyleSheet('background:white')
         
     def initUI(self):
+        # 버튼
         dirbtn = QPushButton('디렉터리 선택', self)
-        dirbtn.resize(100, 50)
+        dirbtn.resize(110, 50)
         dirbtn.move(30, 650)
         dirbtn.setStyleSheet('background:white')
         dirbtn.clicked.connect(self.LoadDir)
@@ -36,33 +41,53 @@ class Canvas(QLabel):
         nextbtn.setStyleSheet('background:white')
         nextbtn.clicked.connect(self.BtnClickedNext)
 
+        # 레이블
         dog = QRadioButton('Dog', self)
         dog.move(800, 30)
         dog.clicked.connect(self.DogChecked)
 
+        dogcolor = QLabel(self)
+        dogcolor.move(860, 30)
+        dogcolor.resize(20,20)
+        dogcolor.setStyleSheet('background:red')
+
         cat = QRadioButton('Cat', self)
-        cat.move(800, 60)
+        cat.move(800, 70)
         cat.clicked.connect(self.CatChecked)
 
-    def LoadDir(self):
-        fname = QFileDialog.getExistingDirectory(self, 'Open File', '', QFileDialog.ShowDirsOnly)
-        
-        if fname:
-            pixmap = QPixmap(fname)
-            pixmap = pixmap.scaled(700, 600)
+        catcolor = QLabel(self)
+        catcolor.move(860, 70)
+        catcolor.resize(20,20)
+        catcolor.setStyleSheet('background:blue')
 
-            self.ImgLabel.setPixmap(pixmap)
-            self.ImgLabel.resize(pixmap.width(), pixmap.height())
+    # 디렉터리 선택
+    def LoadDir(self):
+        fpath = QFileDialog.getExistingDirectory(self, 'Open File', '', QFileDialog.ShowDirsOnly)
+
+        if fpath:
+            pixmap = [QPixmap(fpath+'/'+img).scaled(700, 600) for img in os.listdir(fpath)]
+            pixmap[0] = pixmap[0].scaled(700,600)
+            self.fPixmap = pixmap
+
+            self.ImgLabel.setPixmap(pixmap[0])
+            self.ImgLabel.resize(pixmap[0].width(), pixmap[0].height())
 
             self.show()
 
-            self.dirlabel.setText(fname)
+            # 디렉터리 경로 출력
+            self.dirlabel.setText(fpath)
 
     def BtnClickedPre(self):
         pass
 
     def BtnClickedNext(self):
-        pass
+        self.cnt += 1
+
+        # 이미지 로드 하지 않고 누르면 오류남
+        if self.cnt == len(self.fPixmap):
+            self.cnt = 0
+
+        self.ImgLabel.setPixmap(self.fPixmap[self.cnt])
 
     def DogChecked(self):
         pass
