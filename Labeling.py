0230156addeeecf5773bb29aacfcc4c7cd557f3f
heeyunkwon <76425337+heeyunkwon@users.noreply.github.com>
2021-01-07 00:17:05 +0900

    Update Labeling.py
    파일명 정렬, 바운딩 박스 Update

--- Labeling.py
+++ Labeling.py
@@ -1,4 +1,5 @@
 import sys, os
+import natsort # 숫자 정렬용 라이브러리
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
@@ -8,20 +9,45 @@ class Canvas(QLabel):
         super().__init__()
 
         self.initUI()
+
+        self.begin = QPoint()
+        self.labelSize = QSize(50, 30)
+        self.color = Qt.black
+        self.count = 0
+
+        # 이미지 이동에 필요한 변수
         self.cnt = 0
         self.fPixmap = QPixmap()
 
         # 이미지 레이블
         self.ImgLabel = QLabel(self)
-        self.ImgLabel.move(30, 30)
+        self.ImgLabel.move(35, 35)
 
         # 디렉터리 경로 표시 레이블
         self.dirlabel = QLabel(self)
-        self.dirlabel.resize(500, 50)
+        self.dirlabel.resize(510, 50)
         self.dirlabel.move(150, 650)
-        self.dirlabel.setStyleSheet('background:white')
-        
+        self.dirlabel.setStyleSheet('background:white; border-style:solid; border-width:2px; border-color:black')
+
+        # 레이블 이름
+        self.dogLabel = QLabel('Dog', self.ImgLabel)
+        self.dogLabel.setFont(QFont('Arial', 14))
+        self.dogLabel.setStyleSheet('Color:red')
+        self.dogLabel.resize(0, 0)
+        self.dogLabel.move(50, 50)
+
+        self.catLabel = QLabel('Cat', self.ImgLabel)
+        self.catLabel.setFont(QFont('Arial', 14))
+        self.catLabel.setStyleSheet('Color:blue')
+        self.catLabel.resize(0, 0)
+        self.catLabel.move(50, 50)
+
     def initUI(self):
+        label = QLabel(self)
+        label.resize(700, 600)
+        label.move(30, 30)
+        label.setStyleSheet('background:white; border-style:solid; border-width:2px; border-color:black')
+
         # 버튼
         dirbtn = QPushButton('디렉터리 선택', self)
         dirbtn.resize(110, 50)
@@ -34,39 +60,85 @@ class Canvas(QLabel):
         prebtn.move(670, 650)
         prebtn.setStyleSheet('background:white')
         prebtn.clicked.connect(self.BtnClickedPre)
+        prebtn.setShortcut('LEFT')
 
         nextbtn = QPushButton('>', self)
         nextbtn.resize(100, 50)
         nextbtn.move(770, 650)
         nextbtn.setStyleSheet('background:white')
         nextbtn.clicked.connect(self.BtnClickedNext)
+        nextbtn.setShortcut('RIGHT')
 
         # 레이블
         dog = QRadioButton('Dog', self)
-        dog.move(800, 30)
+        dog.move(770, 40)
         dog.clicked.connect(self.DogChecked)
-
+        
         dogcolor = QLabel(self)
-        dogcolor.move(860, 30)
+        dogcolor.move(830, 40)
         dogcolor.resize(20,20)
         dogcolor.setStyleSheet('background:red')
 
         cat = QRadioButton('Cat', self)
-        cat.move(800, 70)
+        cat.move(770, 80)
         cat.clicked.connect(self.CatChecked)
 
         catcolor = QLabel(self)
-        catcolor.move(860, 70)
+        catcolor.move(830, 80)
         catcolor.resize(20,20)
         catcolor.setStyleSheet('background:blue')
+
+    def mouseButtonKind(self, buttons):
+        if buttons & Qt.RightButton:
+            pass
+
+    def mousePressEvent(self, e):
+        self.begin = e.pos() - QPoint(35, 35)
+
+        if self.count == 1:
+            self.dogLabel.setGeometry(QRect(self.begin - QPoint(0, 35), self.labelSize))
+        
+        elif self.count == 2:
+            self.catLabel.setGeometry(QRect(self.begin - QPoint(0, 35), self.labelSize))
+            
+        self.ImgLabel.update()
+
+    def mouseMoveEvent(self, e):
+        pass
+
+    def Box(self, e):
+        t_pixmap = self.ImgLabel.pixmap()
+        t_pixmap = t_pixmap.copy(0, 0, t_pixmap.width(), t_pixmap.height())
+        painter = QPainter(self.ImgLabel.pixmap())
+        painter.setPen(QPen(QColor(self.color), 5))
+        painter.drawRect(QRect(self.begin, e.pos() - QPoint(35, 35)))
+        painter.end()        
+        self.ImgLabel.repaint()
+        self.ImgLabel.setPixmap(t_pixmap)
+
+    def mouseReleaseEvent(self, e):
+        if self.count == 1:
+            Dog = QPainter(self.ImgLabel.pixmap())
+            Dog.setPen(QPen(QColor(self.color), 5))
+            Dog.drawRect(QRect(self.begin, e.pos() - QPoint(35, 35)))
+            Dog.end()            
+            self.ImgLabel.repaint()
+
+        elif self.count == 2:
+            Cat = QPainter(self.ImgLabel.pixmap())
+            Cat.setPen(QPen(QColor(self.color), 5))
+            Cat.drawRect(QRect(self.begin, e.pos() - QPoint(35, 35)))
+            Cat.end()
+            self.ImgLabel.repaint()
 
     # 디렉터리 선택
     def LoadDir(self):
         fpath = QFileDialog.getExistingDirectory(self, 'Open File', '', QFileDialog.ShowDirsOnly)
 
         if fpath:
-            pixmap = [QPixmap(fpath+'/'+img).scaled(700, 600) for img in os.listdir(fpath)]
-            pixmap[0] = pixmap[0].scaled(700,600)
+            fileList = natsort.natsorted(os.listdir(fpath))
+            pixmap = [QPixmap(fpath+'/'+img).scaled(690, 590) for img in fileList]
+            pixmap[0] = pixmap[0].scaled(690,590)
             self.fPixmap = pixmap
 
             self.ImgLabel.setPixmap(pixmap[0])
@@ -77,16 +149,21 @@ class Canvas(QLabel):
             # 디렉터리 경로 출력
             self.dirlabel.setText(fpath)
 
+    # 저장
+    def Save(self):
+        pass
+
+    # 이전 이미지로 이동
     def BtnClickedPre(self):
         # 이미지 로드 하지 않고 누르면 오류남
-        if self.cnt == -1:
+        if self.cnt < 0:
             self.cnt = len(self.fPixmap) - 1
 
         self.cnt -= 1
 
         self.ImgLabel.setPixmap(self.fPixmap[self.cnt])
 
-
+    # 다음 이미지로 이동
     def BtnClickedNext(self):
         self.cnt += 1
 
@@ -97,10 +174,16 @@ class Canvas(QLabel):
         self.ImgLabel.setPixmap(self.fPixmap[self.cnt])
 
     def DogChecked(self):
-        pass
+        self.color = Qt.red
+        self.count = 1
 
-    def CatChecked(slef):
-        pass
+        self.mouseMoveEvent = self.Box
+
+    def CatChecked(self):
+        self.color = Qt.blue
+        self.count = 2
+
+        self.mouseMoveEvent = self.Box
 
 class MainWindow(QMainWindow):
     def __init__(self):
